7fc69a6bb0ba5b52b5204e6d05e0af39c99dfb20
william-gislan <williammgislanalves@gmail.com>
2025-06-13 17:31:48 -0300

    enviando o projeto

--- db.php
+++ db.php
@@ -1,14 +1,13 @@
-<?php 
- 
-    $dsn = 'mysql:host=localhost;dbname=controle_de_caixasdb';
-    $user = 'root';
-    $pass = 'root';
+<?php
 
-    try{
-        $db = new PDO($dsn, $user, $pass);
-        echo "conectou";
-    } catch (PDOException $ex){
-        echo $ex->getMessage();
-    };
+$dsn = 'mysql:host=localhost;dbname=controle_de_caixasdb';
+$user = 'root';
+$pass = 'agil';
 
-?>
+try {
+
+    $db = new PDO($dsn, $user, $pass);
+    echo "conectou";
+} catch (PDOException $ex) {
+    echo $ex->getMessage();
+};
--- index-action.php
+++ index-action.php
@@ -19,16 +19,45 @@ if($busca){
 };
 $caixas->execute();
 
-
 $todos = $caixas;
+
 $limit = $todos->rowCount();
 
 $p = 1;
 $offset = 0;
 
 if($rpp != 'todos'){
+    if(!$rpp){
+        $limit = 10;
+    } else{
+        $limit = $rpp;
+    }
 
-};
+    $pagina = filter_input(INPUT_GET, 'p', FILTER_DEFAULT);
+    if(!$pagina){
+        $p = 1;
+    } else{
+        $p = $pagina;
+    }
 
-$qt_registros = $limit;
-$qt_paginas = $qt_registros / $limit;
+    $offset = $p - 1;
+    $offset = $offset * $limit;
+
+    $caixas = $db->prepare("$busca_todos LIMIT $offset, $limit");
+
+    if($busca){
+        $caixas->bindValue(":busca", $busca);
+        
+    }
+    $caixas->execute();
+    
+    $todos = $db->prepare($busca_todos);
+    if($busca){
+        $caixas->bindValue(":busca", $busca);
+    }
+    
+    $todos->execute();
+}
+
+//$qt_registros = $limit->rowCount();
+//$qt_paginas = $qt_registros > 0 ? ceil($qt_registros / $limit): 0;
